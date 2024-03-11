e350b78ea6a924855c71ec8236c06a5a756e4489
MacarenaSanchez22 <163000450+MacarenaSanchez22@users.noreply.github.com>
2024-03-11 10:55:01 -0300

    Add files via upload

---
+++ con_db.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+<meta charset="UTF-8">
+<title>Primera pagina</title>
+</head> 
+<body>
+
+<p>Bienvenidos</p>
+<p>Esta es la pagina <b>Diseño y Animacion UB</b> </p>
+<p>aqui aprenderas a hacer los mejores videojuegos</p>
+
+</body>
+</html>
